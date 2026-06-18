e8200492e9b66f5ec6547e088147be2a62d97b7e
Rob Dumas <robdumas@gmail.com>
2026-06-17 22:19:46 -0500

    Added git
    Signed-off-by: Rob Dumas <robdumas@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,7 @@
   environment = {
     systemPackages = with pkgs; [
       curl
+      git
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       zsh
     ];
